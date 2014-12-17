3ee5df89bfc351afbbb2e9ef10eacb8021a58e98
pitau07 <julian.decreuse@utbm.fr>
2014-12-17 12:26:30 +0100

    red tresh lower

--- src/colordetection.m
+++ src/colordetection.m
@@ -1,21 +1,23 @@
 function [ Ibinary ] = colordetection( I )
-%COLORDETECTION
-    %   Finds the color of the sign, between white, blue and red
+%return grayscale image after tresholding with blue and red pixels in white 
+   
     I=rgb2hsv(I);
 
     %Color H S V
     %Red H?240 or H?10 S?40 V?30
-    % 0.05<= H <= 0.97 0.3<=S<=1 0.01<=V<= 1;
+    %((I(i,j,1)>=240/255 || I(i,j,1)<=10/255) && I(i,j,2)>=40/255 && I(i,j,3)>=30/255 )
     %Yellow 18?H?45 S?148 V?66
     %Blue 120<H?175 S?127.5 V?20 
     %bluePixels = hueImage > 0.4 & hueImage < 0.7 & valueImage < 0.8;
 
     s=size(I);
+    
+    I(73,246,:)
 
     IR = uint8(zeros(s));
     for i=1:s(1)
     for j=1:s(2)
-        if ((I(i,j,1)>=240/255 || I(i,j,1)<=10/255) && I(i,j,2)>=40/255 && I(i,j,3)>=30/255 )
+        if ((I(i,j,1)>=235/255 || I(i,j,1)<=15/255) && I(i,j,2)>=40/255 && I(i,j,3)>=30/255 )
             IR(i,j,:)=255;
         end
     end
@@ -30,12 +32,20 @@ function [ Ibinary ] = colordetection( I )
     end
     end
 
-    
+    IR=rgb2gray(IR);
+    IB=rgb2gray(IB);
+
     %erosion 
     SE1=[0 1 0; 1 1 1; 0 1 0];
+    se = strel('disk',4);
     IR=imerode(IR,SE1);
     IB=imerode(IB,SE1);
+
+    IR=imclose(IR,se);
+    IB=imclose(IB,se);
+    figure
+    imshow(IR+IB);
    
-    Ibinary=rgb2gray(IR+IB);
+    Ibinary=IB+IR;
     
 end
